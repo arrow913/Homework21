57a613decd716186c9ca61dd1937409d922fe6f5
william <billy.arrasmith@gmail.com>
2021-01-18 21:40:32 -0600

    moved routes folder

---
+++ client/build/precache-manifest.0d38e30900f78f8c4917dda9bdeda3a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9afe8a26cde10b66c45f",
+    "url": "/static/css/main.be37fea8.chunk.css"
+  },
+  {
+    "revision": "9afe8a26cde10b66c45f",
+    "url": "/static/js/main.fc5318aa.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "193b767e333ca6393c61",
+    "url": "/static/js/2.3bb06115.chunk.js"
+  },
+  {
+    "revision": "d8631c5e3e343c2d962091d98add4e88",
+    "url": "/static/media/blur-book-stack-books-bookshelves-590493.d8631c5e.jpg"
+  },
+  {
+    "revision": "9cce47211c26e81f4e1504a09a3cc064",
+    "url": "/index.html"
+  }
+];
